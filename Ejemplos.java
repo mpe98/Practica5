568b52d8432abbaa05a5e6770c71feda6327b2ef
mpe98 <michael.peter@educa.madrid.org>
2021-10-19 19:07:04 +0200

    primer commit

---
+++ Ejemplos.java
@@ -0,0 +1,14 @@
+package variablesDatosOperadores;
+
+public class Ejemplos {
+    public static void main(String[] args) {
+        // Tipos de datos primitivos
+
+            //Números enteros
+        long largo; //ocupa 64 bits (64/8=8 byte) en memoria, es decir, numeros muy grandes
+        int entero = 999999999; //32 bits --> 4 bytes
+        short corto; // 16 bits --> 2 bytes
+        byte b; //8 bits
+        int = 12312313;
+    }
+}
