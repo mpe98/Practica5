1e6cadbf66d3dad6c2511ddea61fa37b2b49deb3
mpe98 <michael.peter@educa.madrid.org>
2021-10-21 16:07:22 +0200

    Cambio realizado por Michael

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -2,7 +2,7 @@ package variablesDatosOperadores;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        int número = 2;
+        int número = 2; // Cambio realizado por Michael
         int cuad = número * número;
                 System.out.println("El cuadrado de " + número + " es: " +
                         cuad);
