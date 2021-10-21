df244fbc1b8c58cf72cc6d05887d8fff34311493
jferrer299 <joseph.ferrer@educa.madrid.org>
2021-10-21 16:00:15 +0200

    Cambio realizado por Joseph

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,6 +5,8 @@ public class Ejercicio1 {
         int n1 = 50, n2 = 30;
         int suma = 0;
         suma = n1 + n2;
+	int resta = n2 - n1; //Este es el cambio realizado por Joseph
         System.out.println("La suma es: " + suma);
     }
+
 }
